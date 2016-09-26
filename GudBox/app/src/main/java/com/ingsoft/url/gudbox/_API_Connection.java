47db2a37c843ac0bb13b3eeefaf8b8068a76b4dc
cancinos <cancinos7guatemala@gmail.com>
2016-09-26 15:23:40 -0600

    Adding constructor and connect function

--- GudBox/app/src/main/java/com/ingsoft/url/gudbox/_API_Connection.java
+++ GudBox/app/src/main/java/com/ingsoft/url/gudbox/_API_Connection.java
@@ -1,9 +1,52 @@
 package com.ingsoft.url.gudbox;
 
+import android.os.AsyncTask;
+
+import java.io.BufferedInputStream;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.io.IOException;
+import java.net.URLConnection;
+
 /**
  * Created by iiscancinos on 25/09/2016.
  */
 
-public class _API_Connection {
-    
+public abstract class _API_Connection {
+    /***
+     *  Global variables
+     */
+    String main_URL = "http://urlayd.azurewebsites.net/";
+
+    public _API_Connection(){
+        try {
+            connect("api/GetSeeds");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /***
+     * API connection method
+     * @param function_url
+     * @return
+     */
+    public Boolean connect(String function_url) throws IOException {
+        Boolean flag = false;
+        URL url = new URL(main_URL + function_url);
+        HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+
+        try{
+            InputStream input = new BufferedInputStream(urlConnection.getInputStream());
+            int a = 0;
+        }
+        finally{
+            urlConnection.disconnect();
+        }
+
+        return flag;
+    }
+
 }
