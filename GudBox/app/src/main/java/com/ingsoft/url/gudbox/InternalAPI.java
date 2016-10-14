9da881c801c28bbb9da42a021af6b212c63b3914
Pablo Quemé <pgqueme@gmail.com>
2016-10-13 19:58:55 -0600

    Fixing table creation with version from ORM

--- GudBox/app/src/main/java/com/ingsoft/url/gudbox/InternalAPI.java
+++ GudBox/app/src/main/java/com/ingsoft/url/gudbox/InternalAPI.java
@@ -19,13 +19,13 @@ public class InternalAPI {
     public void downloadSeeds(){
         List<ServerSeed> serverSeeds = new ArrayList<ServerSeed>();
         //server magic
-        ServerSeed seed1 = new ServerSeed(1, 0, 0, 90, 0, 0, 0, 0, 4, 1, "Semilla 1", "compost", "test 1", false, false, false, true, true, 7.1);
+        /*ServerSeed seed1 = new ServerSeed(1, 0, 0, 90, 0, 0, 0, 0, 4, 1, "Semilla 1", "compost", "test 1", false, false, false, true, true, 7.1);
         ServerSeed seed2 = new ServerSeed(1, 0, 0, 90, 0, 0, 0, 0, 5, 2, "Semilla 2", "compost", "test 2", false, false, false, true, true, 7.1);
         ServerSeed seed3 = new ServerSeed(1, 0, 0, 90, 0, 0, 0, 0, 6, 3, "Semilla 3", "compost", "test 3", false, false, false, true, true, 7.1);
 
         serverSeeds.add(seed1);
         serverSeeds.add(seed2);
-        serverSeeds.add(seed3);
+        serverSeeds.add(seed3);*/
 
         saveUnsavedSeeds(serverSeeds);
     }
